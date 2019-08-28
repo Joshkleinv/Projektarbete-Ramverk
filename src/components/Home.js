41454e5de0cbbc7b357e79186cadb7244cd6da0b
pnilssson <p.nilsson95@hotmail.com>
2019-08-28 16:58:33 +0200

    Homepage and registration first phase created, routing added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './Home.css';
+import {Container, Button, Header } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
 
 export const Home = () => {
     return (
-        <div>
-
-        </div>
+        <Container className="container">
+            <div className="align-center">
+                <Header as="h2">
+                    Welcome!
+                </Header>
+                <p className="color-white">Please choose if you would like to sign up or login.</p>
+                <Link to="/registration">
+                    <Button
+                        size='large'
+                        color='green'
+                    >
+                        Register account
+                    </Button>
+                </Link>
+                <Link to="/login">
+                    <Button
+                        size='large'
+                        color='green'
+                    >
+                        Login
+                    </Button>
+                </Link>
+            </div>
+        </Container>
     )
 };
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,47 +1,94 @@
 import React from 'react';
-import { Button, Form} from "semantic-ui-react";
+import './Registration.css'
+import { Link } from "react-router-dom";
+import { Container, Divider, Form, Select, Header } from "semantic-ui-react";
 
 class Registration extends React.Component {
     state = {
         firstName: '',
         lastName: '',
         emailAddress: '',
-        password: ''
+        password: '',
+        passwordConfirmation: ''
     };
 
     render() {
+        const options = [
+            { key: 'm', text: 'Male', value: 'male' },
+            { key: 'f', text: 'Female', value: 'female' },
+            { key: 'o', text: 'Other', value: 'other' },
+        ];
+
         return (
-            <div>
-                <Form>
-                    <Form.Group required>
+            <Container>
+                <Form widths='equal'>
+                    <Header as="h2">
+                        Create new account
+                    </Header>
+                    <Divider />
+                    <Form.Group>
                         <Form.Input
+                            required
                             label="First Name"
                             placeholder="First Name"
                             name="firstName"
-                            value={firstName}
-                        />
-                        <Form.Input
-                            label="Last Name"
-                            placeholder="Last Name"
-                            name="lastName"
-                            value={lastName}
-                        />
-                    </Form.Group>
-                    <Form.Group required>
-                        <Form.Input
-                            label="Email Address"
-                            placeholder="Email Address"
-                            name="emailAddress"
-                            value={emailAddress}
                         />
                     </Form.Group>
                     <Form.Group>
-                        
+                        <Form.Input
+                            required
+                            label="Last Name"
+                            placeholder="Last Name"
+                            name="lastName"
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Input
+                            required
+                            className="white-text"
+                            label="Email Address"
+                            placeholder="Email Address"
+                            name="emailAddress"
+                            type="email"
+                        />
+                        <Form.Field
+                            control={Select}
+                            label='Gender'
+                            options={options}
+                            placeholder='Gender'
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Input
+                            required
+                            label="Password"
+                            name="password"
+                            type="password"
+                        />
+                        <Form.Input
+                            required
+                            label="Confirm Password"
+                            name="passwordConfirmation"
+                            type="password"
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Button
+                            color='green'
+                            content='Submit'
+                            type="submit"
+                        />
+                        <Link to="/">
+                            <Form.Button
+                                color='red'
+                                content="Cancel"
+                            />
+                        </Link>
                     </Form.Group>
                 </Form>
-            </div>
+            </Container>
         )
     }
-};
+}
 
 export default Registration;
