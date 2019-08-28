4d502a5b603e675cfd019f3490f6e9a505c214f2
Joshua Klein Velderman <99joskle@mk.se>
2019-08-28 13:51:29 +0200

    Login skeleton

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import { addActionCreator, subtractActionCreator, addAnyActionCreator, clearHistoryActionCreator, multiplyActionCreator } from '../actions'
+import Button from '@material-ui/core/Button';  
 
-export const Login = () => {
-    return (
-        <div>
+class Login extends React.Component{
+    state = {
+        emailAddress: '',
+        password: ''
+    }
 
-        </div>
+    render(){
+        return (
+            <div>
+                <Form>
+                    <Form.Group required>
+                        <Form.Input
+                            label="Email Address"
+                            placeholder="Email Address"
+                            name="emailAddress"
+                            value={emailAddress}
+                        />
+                        <Form.Input
+                            label="Password"
+                            placeholder="Password"
+                            name="password"
+                            value={password}
+                        />
+                        <Form.Button
+                            Button="btnLogin"
+                            placeholder="Login"
+                            name="btnLogin"
+                            value={btnLogin}
+                        />
+                    </Form.Group>
+                </Form>
+            </div>
     )
+    }
 };
+
+export default Login;
