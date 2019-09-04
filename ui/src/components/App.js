a78e9ffe8dac3c0e605b704f34b9f83d6bb99422
Joshua Klein Velderman <99joskle@mk.se>
2019-09-04 13:47:18 +0200

    navbar changes

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import { News } from './News'
-import { Test } from './Test'
+import Navbar from './Navbar'
 import './App.css'
 
 
--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import './Navbar.css'
+import { Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
-export const Navbar = () => {
-  return (
-      <div>
 
-      </div>
-  );
-};
+class Navbar extends React.Component {
+  
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu>
+        <Link to="/friends">
+        <Menu.Item
+          name='Friends'
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        >
+          Friends
+        </Menu.Item>
+        </Link>
+        <Link to="/chat">
+      <Menu.Item
+        name='Chat'
+        active={activeItem === 'chat'}
+        onClick={this.handleItemClick}
+      >
+        Chat
+      </Menu.Item>
+      </Link>
+      <Link to="/news">
+      <Menu.Item
+        name='News'
+        active={activeItem === 'news'}
+        onClick={this.handleItemClick}
+      >
+        News
+      </Menu.Item>
+      </Link>
+      <Menu.Menu position='right'>
+        <Menu.Item
+          name='Logout'
+          active={activeItem === 'logout'}
+          onClick={this.handleItemClick}
+        >
+          Logout
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+    )
+  }
+}
+export default Navbar;
